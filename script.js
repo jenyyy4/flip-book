79b4375daca99504fe7ff92d8a71fa29d05424d3
jenyyy4 <anuskaroy0410@gmail.com>
2025-02-19 23:32:12 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("close-book").addEventListener("click", () => {
+    document.getElementById("checkbox-cover").checked = false;
+    document.getElementById("checkbox-page1").checked = false;
+    document.getElementById("checkbox-page2").checked = false;
+    document.getElementById("checkbox-page3").checked = false;
+    document.getElementById("checkbox-page4").checked = false;
+    document.getElementById("checkbox-page5").checked = false;
+    document.getElementById("checkbox-page6").checked = false;
+    document.getElementById("checkbox-page7").checked = false;
+    document.getElementById("checkbox-page8").checked = false;
+    document.getElementById("checkbox-page9").checked = false;
+    document.getElementById("checkbox-page10").checked = false;
+});
